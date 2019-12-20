6cc48a814c68def8217ccc59d9688e4b7c77d681
Anmol <anmol.mehta018@gmail.com>
2019-12-20 17:30:24 +0530

    reducer intro

--- src/screens/SquareScreenWithReducer.js
+++ src/screens/SquareScreenWithReducer.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import { View, Text, StyleSheet, } from "react-native";
 import ColorCounter from "./ColorCounter";
+import { stat } from "fs";
 
 
 const reducer = (state, action) => {
@@ -18,34 +19,35 @@ const reducer = (state, action) => {
             return { ...state, blue: state.blue + action.amount };
         default:
             return state;
-
     }
 }
 
 const SquareScreen = () => {
 
 
+    const COLOR_INCREMENT = 15;
+
     const [state, dispatch] = useReducer(reducer, { 'red': 0, 'green': 0, 'blue': 0 });
 
-
+    const [colorRed, colorGreen, colorblue] = state;
 
     return <View>
         <Text>SQUARE SCREEN</Text>
 
         <ColorCounter
-            onIncrease={() => { }}
-            onDecrease={() => { }}
+            onIncrease={() => dispatch({ colorTochange: 'red', amount: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ colorTochange: 'red', amount: -1 * COLOR_INCREMENT })}
             color='Red'
         />
         <ColorCounter
             color='Green'
-            onIncrease={() => { }}
-            onDecrease={() => { }}
+            onIncrease={() => dispatch({ colorTochange: 'green', amount: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ colorTochange: 'green', amount: -1 * COLOR_INCREMENT })}
         />
         <ColorCounter
             color='Blue'
-            onIncrease={() => { }}
-            onDecrease={() => { }}
+            onIncrease={() => dispatch({ colorTochange: 'blue', amount: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ colorTochange: 'blue', amount: -1 * COLOR_INCREMENT })}
         />
 
 
